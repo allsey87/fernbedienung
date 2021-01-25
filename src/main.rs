7d074b8b0656af78af11b77767db97d370d6434f
Michael Allwright <allsey87@gmail.com>
2021-01-25 14:58:28 +0100

    Upgrade protocol (work in progress)

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,8 @@ use tokio::net::TcpListener;
 use tokio::io::AsyncWriteExt;
 use tokio_serde::{SymmetricallyFramed, formats::SymmetricalJson};
 use tokio_util::codec::{Decoder, FramedRead, FramedWrite, LengthDelimitedCodec};
-
 use bytes::BytesMut;
+use uuid::Uuid;
 
 mod process;
 
@@ -32,34 +32,42 @@ struct Run {
 }
 
 #[derive(Debug, Deserialize)]
-enum Request {
+enum RequestKind {
     Ping,
     Upload(Upload),
+    /* it feels more and more like these two can be merged */
     Run(Run),
-    Process(u32, process::Request),
+    Process(Uuid, process::Request),
+}
+
+#[derive(Debug, Deserialize)]
+struct Request(Uuid, RequestKind);
+
+#[derive(Debug, Serialize)]
+enum ResponseKind {
+    Ok,
+    Error(String),
+    Process(Uuid, process::Response),
 }
 
 #[derive(Debug, Serialize)]
-enum Response {
-    Ok,
-    Error(String),
-    Process(u32, process::Response),
-}
+struct Response(Uuid, ResponseKind);
 
-impl<T, E: std::fmt::Display> std::convert::From<Result<T, E>> for Response {
-    fn from(result: Result<T, E>) -> Self {
-        match result {
-            Ok(_) => Response::Ok,
-            Err(error) => Response::Error(error.to_string())
-        }
+
+impl<T, E: std::fmt::Display> std::convert::From<(uuid::Uuid, Result<T, E>)> for Response {
+    fn from((uuid, result): (Uuid, Result<T, E>)) -> Self {
+        Response(uuid, match result {
+            Ok(_) => ResponseKind::Ok,
+            Err(error) => ResponseKind::Error(error.to_string())
+        })
     }
 }
 
-struct ProcessCodec(u32, process::Source);
+struct ProcessCodec(Uuid, process::Source);
 
 impl ProcessCodec {
-    fn new(pid: u32, source: process::Source) -> ProcessCodec {
-        ProcessCodec(pid, source)
+    fn new(id: Uuid, source: process::Source) -> ProcessCodec {
+        ProcessCodec(id, source)
     }
 }
 
@@ -70,7 +78,7 @@ impl Decoder for ProcessCodec {
     fn decode(&mut self, buf: &mut BytesMut) -> Result<Option<Response>, io::Error> {
         if !buf.is_empty() {
             let length = buf.len();
-            let response = Response::Process(self.0, process::Response::Output(self.1, buf.split_to(length)));
+            let response = Response(self.0, ResponseKind::Process(self.0, process::Response::Output(self.1, buf.split_to(length))));
             Ok(Some(response))
         } else {
             Ok(None)
@@ -93,7 +101,7 @@ pub async fn main() {
         tokio::spawn(async move {
             // TODO security isn't important for this application, move this hashmap out
             // of the client and protect with RwLock?
-            let mut process_tx : HashMap<u32, UnboundedSender<process::Request>> = HashMap::new();
+            let mut process_tx : HashMap<Uuid, UnboundedSender<process::Request>> = HashMap::new();
             let mut processes = FuturesUnordered::new();
 
             let (recv, send) = socket.split();
@@ -115,8 +123,8 @@ pub async fn main() {
 
             loop {
                 tokio::select! {
-                    Some((id, exit_result)) = processes.next() => {
-                        process_tx.remove(&id);
+                    Some((uuid, exit_result)) = processes.next() => {
+                        process_tx.remove(&uuid);
                         let exit_result : io::Result<std::process::ExitStatus> = exit_result;
                         // todo, exit result is not necessarily ok
                         if let Ok(exit_result) = exit_result {
@@ -126,7 +134,7 @@ pub async fn main() {
                             else {
                                 process::Response::Terminated(false)
                             };
-                            let response = Response::Process(id, termination);
+                            let response = Response(uuid, ResponseKind::Process(uuid, termination));
                             if let Err(error) = tx.send(response).await {
                                 log::error!("Failed to send terminate message to client: {}", error);
                             }
@@ -138,36 +146,35 @@ pub async fn main() {
                     result = &mut multiplex_encode_task => {
                         log::warn!("multiplex_encode_task: {:?}", result);
                     },
-                    Ok(Some(message)) = requests.try_next() => {
-                        let response = match message {
-                            Request::Ping => {
+                    // is using next instead of try_next better here?
+                    Ok(Some(Request(uuid, request))) = requests.try_next() => {
+                        let response = match request {
+                            RequestKind::Ping => {
                                 log::info!("[{}] received ping", peer_addr);
-                                Response::Ok
+                                Response(uuid, ResponseKind::Ok)
                             },
-                            Request::Upload(upload) => {
+                            RequestKind::Upload(upload) => {
                                 let file_path = upload.path.join(upload.filename);
                                 log::info!("[{}] uploaded {}", peer_addr, file_path.to_string_lossy());
                                 let contents = upload.contents;
-                                tokio::fs::create_dir_all(&upload.path)
-                                    .and_then(|_| tokio::fs::write(&file_path, &contents)).await
-                                    .into()
+                                let result = tokio::fs::create_dir_all(&upload.path)
+                                    .and_then(|_| tokio::fs::write(&file_path, &contents)).await;
+                                Response(uuid, match result {
+                                    Ok(_) => ResponseKind::Ok,
+                                    Err(error) => ResponseKind::Error(error.to_string())
+                                })
                             },
-                            Request::Process(id, request) => {
+                            RequestKind::Process(id, request) => {
                                 log::info!("[{}] request to process {}, {:?}", peer_addr, id, request);
-                                match process_tx.get_mut(&id) {
-                                    Some(process_tx) => {
-                                        match process_tx.send(request).await {
-                                            Ok(_) => Response::Ok,
-                                            Err(_) => Response::Error("Could not communicate with process".to_owned())
-                                        }
-                                        // send back ok for now, alternatively another pipe would have to be constructed
-                                        // and added to map to allow two way communciation. Alternatively, we could
-                                        // just not send anything back, and let the process respond
+                                Response(uuid, match process_tx.get_mut(&id) {
+                                    Some(process_tx) => match process_tx.send(request).await {
+                                        Ok(_) => ResponseKind::Ok,
+                                        Err(_) => ResponseKind::Error("Could not communicate with process".to_owned())
                                     },
-                                    None => Response::Error("No such process".to_owned())
-                                }
+                                    None => ResponseKind::Error("No such process".to_owned())
+                                })
                             }
-                            Request::Run(run) => {
+                            RequestKind::Run(run) => {
                                 let mut process = tokio::process::Command::new(run.target)
                                     .current_dir(run.working_dir)
                                     .args(run.args)
@@ -177,65 +184,60 @@ pub async fn main() {
                                     .spawn()
                                     .expect("failed to start process");
 
-                                if let Some(id) = process.id() {
-                                    /* control channel */
-                                    let (proc_tx, mut proc_rx) = futures::channel::mpsc::unbounded::<process::Request>();
-                                    /* stdout */
-                                    let stdout = process.stdout.take().expect("could not get pipe for stdout");
-                                    let stdout_stream = FramedRead::new(stdout, ProcessCodec::new(id, process::Source::Stdout));
-                                    let mut stdout_forward = stdout_stream.forward(
-                                        tx.clone().sink_map_err(|inner| {
-                                            io::Error::new(io::ErrorKind::BrokenPipe, inner)
-                                        })
-                                    ).fuse();
-                                    /* stderr */
-                                    let stderr = process.stderr.take().expect("could not get pipe for stderr");
-                                    let stderr_stream = FramedRead::new(stderr, ProcessCodec::new(id, process::Source::Stderr));
-                                    let mut stderr_forward = stderr_stream.forward(
-                                        tx.clone().sink_map_err(|inner| {
-                                            io::Error::new(io::ErrorKind::BrokenPipe, inner)
-                                        })
-                                    ).fuse();
-                                    /* stdin */
-                                    let mut stdin = process.stdin.take().expect("could not get pipe for stdin");
-                                    /* process loop */
-                                    processes.push(async move {
-                                        let exit_result : io::Result<std::process::ExitStatus> = loop {
-                                            tokio::select! {
-                                                Some(message) = proc_rx.next() => match message {
-                                                    process::Request::Write(data) => {
-                                                        if let Err(error) = stdin.write_all(&data).await {
-                                                            log::error!("[{}] failed to write to stdin: {}", peer_addr, error);
-                                                        }
-                                                    },
-                                                    process::Request::Kill => { 
-                                                        if let Err(error) = process.start_kill() {
-                                                            log::error!("[{}] failed to start kill: {}", peer_addr, error);
-                                                        }
+                                /* control channel */
+                                let (proc_tx, mut proc_rx) = futures::channel::mpsc::unbounded::<process::Request>();
+                                /* stdout */
+                                let stdout = process.stdout.take().expect("could not get pipe for stdout");
+                                let stdout_stream = FramedRead::new(stdout, ProcessCodec::new(uuid, process::Source::Stdout));
+                                let mut stdout_forward = stdout_stream.forward(
+                                    tx.clone().sink_map_err(|inner| {
+                                        io::Error::new(io::ErrorKind::BrokenPipe, inner)
+                                    })
+                                ).fuse();
+                                /* stderr */
+                                let stderr = process.stderr.take().expect("could not get pipe for stderr");
+                                let stderr_stream = FramedRead::new(stderr, ProcessCodec::new(uuid, process::Source::Stderr));
+                                let mut stderr_forward = stderr_stream.forward(
+                                    tx.clone().sink_map_err(|inner| {
+                                        io::Error::new(io::ErrorKind::BrokenPipe, inner)
+                                    })
+                                ).fuse();
+                                /* stdin */
+                                let mut stdin = process.stdin.take().expect("could not get pipe for stdin");
+                                /* process loop */
+                                processes.push(async move {
+                                    let exit_result : io::Result<std::process::ExitStatus> = loop {
+                                        tokio::select! {
+                                            Some(message) = proc_rx.next() => match message {
+                                                process::Request::Write(data) => {
+                                                    if let Err(error) = stdin.write_all(&data).await {
+                                                        log::error!("[{}] failed to write to stdin: {}", peer_addr, error);
                                                     }
                                                 },
-                                                _ = &mut stdout_forward => {},
-                                                _ = &mut stderr_forward => {},
-                                                // note take out stdin!
-                                                exit_result = process.wait() => {
-                                                    if stdout_forward.is_terminated() &&
-                                                       stderr_forward.is_terminated() {
-                                                        break exit_result;
+                                                process::Request::Kill => { 
+                                                    if let Err(error) = process.start_kill() {
+                                                        log::error!("[{}] failed to start kill: {}", peer_addr, error);
                                                     }
-                                                },
-                                            }
-                                        };
-                                        (id, exit_result)
-                                    });
-                                    /* insert the tx end of the control channel into a map*/
-                                    process_tx.insert(id, proc_tx);
+                                                }
+                                            },
+                                            _ = &mut stdout_forward => {},
+                                            _ = &mut stderr_forward => {},
+                                            // note take out stdin!
+                                            exit_result = process.wait() => {
+                                                if stdout_forward.is_terminated() &&
+                                                    stderr_forward.is_terminated() {
+                                                    break exit_result;
+                                                }
+                                            },
+                                        }
+                                    };
+                                    (uuid, exit_result)
+                                });
+                                /* insert the tx end of the control channel into a map*/
+                                process_tx.insert(uuid, proc_tx);
 
-                                    Response::Process(id, process::Response::Started)
-                                }
-                                else {
-                                    Response::Error("Something went wrong".to_owned())
-                                }
-                            },
+                                Response(uuid, ResponseKind::Process(uuid, process::Response::Started))
+                            }
                         };
 
                         if let Err(error) = tx.send(response).await {
