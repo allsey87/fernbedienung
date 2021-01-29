ae081655d49a98444f2956d4b3662a36c83392ad
Michael Allwright <allsey87@gmail.com>
2021-01-29 15:51:10 +0100

    Move request handling code outside of main

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 use futures::{prelude::*, stream::FuturesUnordered};
 use serde::{Deserialize, Serialize};
-use std::path::PathBuf;
+use std::{net::SocketAddr, path::PathBuf};
 use tokio_util::codec::{FramedRead, FramedWrite, LengthDelimitedCodec};
 use tokio::{io::AsyncWriteExt, net::TcpListener};
 use tokio_serde::{SymmetricallyFramed, formats::SymmetricalJson};
@@ -36,17 +36,6 @@ pub enum ResponseKind {
 
 #[derive(Debug, Serialize)]
 pub struct Response(Option<Uuid>, ResponseKind);
-
-/*
-impl<T, E: std::fmt::Display> std::convert::From<(uuid::Uuid, Result<T, E>)> for Response {
-    fn from((uuid, result): (Uuid, Result<T, E>)) -> Self {
-        Response(uuid, match result {
-            Ok(_) => ResponseKind::Ok,
-            Err(error) => ResponseKind::Error(error.to_string())
-        })
-    }
-}
-*/
 
 #[tokio::main]
 pub async fn main() {
@@ -93,93 +82,98 @@ pub async fn main() {
                     },
                     request = requests.next() => match request {                  
                         Some(request) => {
-                            let response = match request {
-                                Ok(Request(uuid, request)) => match request {
-                                    RequestKind::Ping => {
-                                        log::info!("[{}] ping", peer_addr);
-                                        Response(Some(uuid), ResponseKind::Ok)
-                                    },
-                                    RequestKind::Upload(upload) => {
-                                        let file_path = upload.path.join(upload.filename);
-                                        log::info!("[{}] uploading {}", peer_addr, file_path.to_string_lossy());
-                                        let contents = upload.contents;
-                                        let result = tokio::fs::create_dir_all(&upload.path)
-                                            .and_then(|_| tokio::fs::write(&file_path, &contents)).await;
-                                        Response(Some(uuid), match result {
-                                            Ok(_) => ResponseKind::Ok,
-                                            Err(error) => ResponseKind::Error(error.to_string())
-                                        })
-                                    },
-                                    RequestKind::Process(request) => Response(Some(uuid), match request {
-                                        process::Request::Run(run) => {
-                                            log::info!("[{}] running {:?}", peer_addr, run);
-                                            let process = process::Process::new(tx.clone(), uuid, run);
-                                            processes.push(process);
-                                            ResponseKind::Process(process::Response::Started)
-                                        },
-                                        process::Request::Write(data) => {
-                                            log::info!("[{}] writing {:?}", peer_addr, data);
-                                            let stdin = processes
-                                                .iter_mut()
-                                                .find(|process| process.uuid == uuid)
-                                                .and_then(|process| process.stdin.take());
-                                            match stdin {
-                                                Some(mut input) => {
-                                                    let response = match input.write_all(data.as_ref()).await {
-                                                        Ok(_) => ResponseKind::Ok,
-                                                        Err(error) => ResponseKind::Error(error.to_string())
-                                                    };
-                                                    /* try to put the standard input back */
-                                                    processes
-                                                        .iter_mut()
-                                                        .find(|process| process.uuid == uuid)
-                                                        .map(|process| process.stdin.get_or_insert(input));
-                                                    /* return the response */
-                                                    response
-                                                },
-                                                None => ResponseKind::Error("Standard input unavailable".to_owned())
-                                            }
-                                        },
-                                        process::Request::Signal(number) => {
-                                            log::info!("[{}] signaling {:?}", peer_addr, number);
-                                            let pid = processes
-                                                .iter()
-                                                .find(|process| process.uuid == uuid)
-                                                .and_then(|process| process.pid);
-                                            match pid {
-                                                Some(pid) => {
-                                                    let process = tokio::process::Command::new("kill")
-                                                        .arg(format!("-{}", number))    
-                                                        .arg(format!("{}", pid))
-                                                        .output();
-                                                    match process.await {
-                                                        Ok(std::process::Output {status, ..}) => match status.code() {
-                                                            Some(0) => ResponseKind::Ok,
-                                                            Some(code) => ResponseKind::Error(format!("signal terminated with {}", code)),
-                                                            None => ResponseKind::Error(format!("signal terminated without code"))
-                                                        },
-                                                        Err(error) => ResponseKind::Error(error.to_string()),
-                                                    }
-                                                },
-                                                None => ResponseKind::Error("Could not find process".to_owned())
-                                            }
-                                        }
-                                    })
-                                },
-                                Err(error) => Response(None, ResponseKind::Error(error.to_string()))
-                            };
+                            let response = handle_client_request(request, &peer_addr, &tx, &mut processes).await;
                             if let Err(error) = tx.send(response).await {
                                 log::error!("{}", error.to_string());
                             }
                         },
-                        None => {
-                            /* at this point it is probably the case that the client has disconnected  */
-                            break
-                        },
+                        /* at this point it is probably the case that the client has disconnected */
+                        None => break
                     } // Some(request) = requests.next() => {
                 } // select
             }
             log::info!("discconected from {}", peer_addr);
         }); // tokio::spawn
+    }
+}
+
+async fn handle_client_request(request: std::io::Result<Request>, 
+                               peer_addr: &SocketAddr,
+                               tx: &futures::channel::mpsc::UnboundedSender<Response>,
+                               processes: &mut FuturesUnordered<process::Process>) -> Response {
+    match request {
+        Ok(Request(uuid, request)) => match request {
+            RequestKind::Ping => {
+                log::info!("[{}] ping", peer_addr);
+                Response(Some(uuid), ResponseKind::Ok)
+            },
+            RequestKind::Upload(upload) => {
+                let file_path = upload.path.join(upload.filename);
+                log::info!("[{}] uploading {}", peer_addr, file_path.to_string_lossy());
+                let contents = upload.contents;
+                let result = tokio::fs::create_dir_all(&upload.path)
+                    .and_then(|_| tokio::fs::write(&file_path, &contents)).await;
+                Response(Some(uuid), match result {
+                    Ok(_) => ResponseKind::Ok,
+                    Err(error) => ResponseKind::Error(error.to_string())
+                })
+            },
+            RequestKind::Process(request) => Response(Some(uuid), match request {
+                process::Request::Run(run) => {
+                    log::info!("[{}] running {:?}", peer_addr, run);
+                    let process = process::Process::new(tx.clone(), uuid, run);
+                    processes.push(process);
+                    ResponseKind::Process(process::Response::Started)
+                },
+                process::Request::Write(data) => {
+                    log::info!("[{}] writing {:?}", peer_addr, data);
+                    let stdin = processes
+                        .iter_mut()
+                        .find(|process| process.uuid == uuid)
+                        .and_then(|process| process.stdin.take());
+                    match stdin {
+                        Some(mut input) => {
+                            let response = match input.write_all(data.as_ref()).await {
+                                Ok(_) => ResponseKind::Ok,
+                                Err(error) => ResponseKind::Error(error.to_string())
+                            };
+                            /* try to put the standard input back */
+                            processes
+                                .iter_mut()
+                                .find(|process| process.uuid == uuid)
+                                .map(|process| process.stdin.get_or_insert(input));
+                            /* return the response */
+                            response
+                        },
+                        None => ResponseKind::Error("Standard input unavailable".to_owned())
+                    }
+                },
+                process::Request::Signal(number) => {
+                    log::info!("[{}] signaling {:?}", peer_addr, number);
+                    let pid = processes
+                        .iter()
+                        .find(|process| process.uuid == uuid)
+                        .and_then(|process| process.pid);
+                    match pid {
+                        Some(pid) => {
+                            let process = tokio::process::Command::new("kill")
+                                .arg(format!("-{}", number))    
+                                .arg(format!("{}", pid))
+                                .output();
+                            match process.await {
+                                Ok(std::process::Output {status, ..}) => match status.code() {
+                                    Some(0) => ResponseKind::Ok,
+                                    Some(code) => ResponseKind::Error(format!("signal terminated with {}", code)),
+                                    None => ResponseKind::Error(format!("signal terminated without code"))
+                                },
+                                Err(error) => ResponseKind::Error(error.to_string()),
+                            }
+                        },
+                        None => ResponseKind::Error("Could not find process".to_owned())
+                    }
+                }
+            })
+        },
+        Err(error) => Response(None, ResponseKind::Error(error.to_string()))
     }
 }
